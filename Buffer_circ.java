64ebf1137cd2b8da3ed481a51b26652045c5958f
Unknown <antoinedelise976@hotmail.fr>
2016-11-29 19:05:20 +0100

    buff v1

--- Buffer_circ.java
+++ Buffer_circ.java
@@ -7,11 +7,15 @@ public class Buffer_circ implements Tampon {
 
 	Message[] _buff;
 	int _size;
+	int _S;
+	int _N;
 
 	public Buffer_circ(int size)
 	{
 		_size = size;
 		_buff = new Message[size];
+		_S = 0;
+		_N = 0;
 	}
 
 	@Override
@@ -22,13 +26,34 @@ public class Buffer_circ implements Tampon {
 
 	@Override
 	public Message get(_Consommateur arg0) throws Exception, InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		Message ret;
+		if(_S!=_N)
+		{
+			ret = _buff[_S];
+			_S = (_S+1)%_size;
+			notify();
+			return ret;
+		}
+		else
+		{
+			wait();
+			return get(arg0);
+		}
 	}
 
 	@Override
 	public void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
-		// TODO Auto-generated method stub
+		if(_S!=(_N+1)%_size)
+		{
+			_buff[_N] = arg1;
+			_N = (_N+1)%_size;
+			notify();
+		}
+		else
+		{
+			wait();
+			put(arg0, arg1);
+		}
 
 	}
 
