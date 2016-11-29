a69e321ce58ca20c8908ce08595bfd3ff1172c7d
Unknown <antoinedelise976@hotmail.fr>
2016-11-29 18:21:29 +0100

    drg

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
 
-public class Main {
+public class Main extends Simulateur {
+
+	public Main(Observateur observateur) {
+		super(observateur);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		System.out.print("Hello world!");
+	}
+
+	@Override
+	protected void run() throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
