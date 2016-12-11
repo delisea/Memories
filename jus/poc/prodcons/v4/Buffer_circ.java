812a78b2f9922bf6597a57f17327b1cd60bd7731
Stiglitz <amodrufavin.hugo@gmail.com>
2016-12-11 16:32:42 +0100

    Debut v4

---
+++ jus/poc/prodcons/v4/Buffer_circ.java
@@ -0,0 +1,111 @@
+package jus.poc.prodcons.v4;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons._Producteur;
+
+public class Buffer_circ implements Tampon {
+
+	static public final Object Global_lock = new Object();
+
+	static public int _nc;
+	static public int _np;
+	static public final Object _lockP = new Object();
+	static public final Object _lockC = new Object();
+
+	Message[] _buff;
+	int _size;
+	int _S;
+	int _N;
+	int _att;
+
+	boolean _closed;
+
+	public Buffer_circ(int size)
+	{
+		_size = size;
+		_buff = new Message[size];
+		_S = 0;
+		_N = 0;
+		_att = 0;
+		_np = 0;
+		_nc = 0;
+		_closed = false;
+	}
+
+	public void close()
+	{
+		_closed = true;
+	}
+
+	@Override
+	public int enAttente() {
+		return _att;
+	}
+
+	@Override
+	public Message get(_Consommateur arg0) {
+		Message ret;
+
+		synchronized(_lockC){
+			System.out.println(arg0.identification() + "C: I want read.");
+			if(_att == 0 || _nc > 0)
+			{
+				_nc++;
+				if(_closed)
+					return null;
+
+				try {
+					Buffer_circ._lockC.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if(_att > 1) Buffer_circ._lockC.notify();
+
+		        if(_closed)
+		          return null;
+				_nc--;
+			}
+			_att--;
+			ret = _buff[_S];
+			_S = (_S+1)%_size;
+			System.out.println(arg0.identification() + "C: I read ->" + ret);
+		}
+		synchronized(_lockP){ Buffer_circ._lockP.notify(); }
+		return ret;
+	}
+
+	@Override
+	public void put(_Producteur arg0, Message arg1) {
+
+		synchronized(Buffer_circ._lockP){
+			System.out.println(arg0.identification() + "P: I want produce.");
+
+			if(_size - _att == 0 || _np > 0)
+			{
+				_np++;
+				System.out.println("taken");
+				try {
+					Buffer_circ._lockP.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if(_size - _att > 1) Buffer_circ._lockP.notify();
+				_np--;
+			}
+
+			_att++;
+			_buff[_N] = arg1;
+			_N = (_N+1)%_size;
+			System.out.println(arg0.identification() + "P: I have produced.");
+		}
+		synchronized(_lockC){ Buffer_circ._lockC.notify(); }
+	}
+
+	@Override
+	public int taille() {
+		return _size;
+	}
+
+}
+
---
+++ jus/poc/prodcons/v4/FProd.java
@@ -0,0 +1,121 @@
+package jus.poc.prodcons.v4;
+import jus.poc.prodcons.*;
+
+public class FProd extends Acteur implements _Producteur {
+
+	private static int _processing = 0;
+
+	private synchronized static void add_processing()
+	{
+		_processing++;
+	}
+
+	private synchronized static void remove_processing()
+	{
+		_processing--;
+	}
+
+	public static int get_processing()
+	{
+		return _processing;
+	}
+
+	private static Aleatoire RANDPRODT = new Aleatoire(2, 1);
+	private static Aleatoire RANDPRODM = new Aleatoire(2, 1);
+
+	Buffer_circ _buffer;
+	int _nbM;
+	int _dM;
+
+	Observateur _obs;
+	private static int _TM;
+	private static int _TdM;
+	static int _NbEx;
+	static int _dNbEx;
+	
+	public FProd(Buffer_circ buffer, Observateur observateur) throws ControlException
+	{
+		super(Acteur.typeProducteur, observateur, _TM, _TdM);
+		observateur.newProducteur(this);
+	    _nbM = RANDPRODM.next();
+		_buffer = buffer;
+		_obs = observateur;
+	}
+
+	  public static void init(int moyenneTempsDeTraitement, int deviationTempsDeTraitement, int nombreMoyenDeProduction, int deviationNombreDeProduction, int nombreMoyenExemplaires, int deviationNombreExemplaires){
+	    RANDPRODT = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+	    RANDPRODM = new Aleatoire(nombreMoyenDeProduction, deviationNombreDeProduction);
+		_TM = moyenneTempsDeTraitement;
+		_TdM = deviationTempsDeTraitement;
+		_NbEx = nombreMoyenExemplaires;
+		_dNbEx = deviationNombreExemplaires;
+	}
+
+	protected void produce() throws ControlException
+	{
+		GMessage message = new GMessage(nombreDeMessages() + ";Hi! I'm " + identification());
+		int delai = RANDPRODT.next()*1000;
+		observateur.productionMessage(this, message, delai);
+		observateur.depotMessage(this,  message);
+		_buffer.put(this, message);
+		_nbM--;
+		try {
+			sleep(delai);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public int deviationTempsDeTraitement() {
+		return super.deviationTempsDeTraitement;
+	}
+
+	@Override
+	public int identification() {
+		return super.identification();
+	}
+
+	@Override
+	public int moyenneTempsDeTraitement() {
+		return super.moyenneTempsDeTraitement;
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return _nbM;
+	}
+
+	@Override
+	public void run() {
+		add_processing();
+
+		try {
+			TestProdCons.getThr().join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(identification() + "P: je démarre et j'ai " + _nbM + " paquets.");
+		while(_nbM>0)
+		{
+			try {
+				produce();
+			} catch (ControlException e) {
+				e.printStackTrace();
+			}
+		}
+
+		remove_processing();
+
+	    if(get_processing() == 0)
+	    {
+	      _buffer.close();
+	      synchronized(Buffer_circ._lockC)
+	      {
+	        Buffer_circ._lockC.notifyAll();
+	      }
+	    }
+	    System.out.println(identification() + "P: je part.");
+	}
+
+}
